beaec7ce956ecd58cc63150d2cf3f428f23b2812
GagandeepSharma060 <gagandeepsharma2488@gmail.com>
2023-12-02 22:53:00 +0530

    first

---
+++ frontend/src/components/others/profile.js
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from "react";
+import { Box, Text, Button, Container, useToast } from "@chakra-ui/react";
+import Navbar from "./navbar";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const ProfilePage = () => {
+  const navigate = useNavigate();
+  const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+ 
+  const toast = useToast();
+
+  
+  const user = {
+    username: userInfo.name,
+    email: userInfo.email,
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem("userInfo");
+    navigate("/", { replace: "true" });
+  };
+
+  
+
+  return (
+    <>
+      <Navbar />
+      <Container maxW="lg" py={8} mt={10}>
+        <Box
+          borderWidth="1px"
+          p={4}
+          borderRadius="md"
+          background="white"
+          textAlign="center"
+          mt={5}
+        >
+          <Text fontSize="3xl" fontWeight="bold">
+            Profile
+          </Text>
+          <Text mt={2} fontSize="xl">
+            Role: {userInfo.isTeacher ? <>Teacher</> : <>Student</>}
+          </Text>
+          <Text fontSize="xl">Name: {user.username}</Text>
+          <Text fontSize="xl">Email: {user.email}</Text>
+          <Button colorScheme="red" mt={5} onClick={handleLogout}>
+            Logout
+          </Button>
+        </Box>
+       
+      
+      </Container>
+    </>
+  );
+};
+
+export default ProfilePage;
